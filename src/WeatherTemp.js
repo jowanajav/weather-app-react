43158c38d951f71f129fbbe1361aff183edafaa5
jowanajav <72789389+jowanajav@users.noreply.github.com>
2021-12-19 18:34:05 +0900

    removed unit conversion

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("celcius");
+  // const [unit, setUnit] = useState("celcius");
   // function showFahrenheit(event) {
   //   event.preventDefault();
   //   setUnit("fahrenheit");
   // }
 
-  function showCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
+  // function showCelcius(event) {
+  //   event.preventDefault();
+  //   setUnit("celcius");
+  // }
 
   // function fahrenheitTemp() {
   //   return (props.celcius * 9) / 5 + 32;
